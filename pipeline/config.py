4f78326882a196a23622c6db3f6728dcce824075
Rahul Ray <rahulraypm2002@gmail.com>
2026-04-19 14:11:14 +0200

    feat: expand to 34 RSS feeds, raise article cap to 5000

--- pipeline/config.py
+++ pipeline/config.py
@@ -1,20 +1,55 @@
 import os
 
 RSS_FEEDS = [
+    # general news
     "http://feeds.bbci.co.uk/news/rss.xml",
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
     "https://news.google.com/rss/search?q=when:24h+allinurl:reuters.com&ceid=US:en&hl=en-US&gl=US",
     "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
     "https://www.theguardian.com/world/rss",
+    "https://www.theguardian.com/us-news/rss",
+    "https://feeds.npr.org/1001/rss.xml",
+    "https://feeds.npr.org/1004/rss.xml",
+    "https://abcnews.go.com/abcnews/topstories",
+    "https://feeds.skynews.com/feeds/rss/home.xml",
+    "https://www.aljazeera.com/xml/rss/all.xml",
+    "https://feeds.washingtonpost.com/rss/world",
+    "https://feeds.washingtonpost.com/rss/national",
+
+    # technology
     "https://feeds.arstechnica.com/arstechnica/index",
     "https://techcrunch.com/feed/",
+    "https://www.wired.com/feed/rss",
+    "https://www.theverge.com/rss/index.xml",
+    "https://feeds.feedburner.com/venturebeat/SZYF",
+
+    # science
     "https://www.sciencedaily.com/rss/top/science.xml",
+    "https://www.newscientist.com/feed/home/",
+    "https://www.nature.com/nature.rss",
+
+    # business and finance
     "https://feeds.bloomberg.com/markets/news.rss",
     "https://www.ft.com/?format=rss",
+    "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
+    "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml",
+    "https://fortune.com/feed/",
+
+    # sports
     "https://www.espn.com/espn/rss/news",
+    "https://www.skysports.com/rss/12040",
+
+    # health
     "https://rss.medicalnewstoday.com/featurednews.xml",
+    "https://www.health.com/syndication/rss/content.xml",
+
+    # politics
+    "https://thehill.com/rss/syndicator/19110",
+    "https://feeds.politico.com/politico/rss/politicopicks",
 ]
 
-MAX_ARTICLES              = 3000
+MAX_ARTICLES              = 5000
 MIN_ARTICLES              = 300
 DAYS_BACK                 = 7
 
@@ -49,11 +84,9 @@ DIGEST_PATH               = "digest.html"
 
 SITE_URL                  = "https://rahulrayy.github.io/newsphere-live"
 
-RESEND_API_KEY            = os.environ.get("RESEND_API_KEY")
-RESEND_AUDIENCE_ID        = os.environ.get("RESEND_AUDIENCE_ID")
-RESEND_FROM_EMAIL         = os.environ.get("RESEND_FROM_EMAIL", "digest@example.com")
+BREVO_API_KEY             = os.environ.get("BREVO_API_KEY")
 
 CLOUDFLARE_WORKER_URL     = os.environ.get(
     "CLOUDFLARE_WORKER_URL",
-    "https://newsphere-subscribe.YOUR_SUBDOMAIN.workers.dev",
+    "https://newsphere-subscribe.rahulraypm2002.workers.dev",
 )
